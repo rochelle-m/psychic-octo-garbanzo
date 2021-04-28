667a2203ff403d9b8be2c9e39350a11a09b95d67
rochelle-m <mca.1930@unigoa.ac.in>
2021-04-28 11:03:30 +0530

    changes

--- HenloWorld.java
+++ HenloWorld.java
@@ -1,5 +1,9 @@
+import java.util.Date;
+
 class HenloWorld {
 	public static void main(String[] args) {
 		System.out.println("Henlo World!");
+		Date date = new Date();
+		System.out.println(date);
 	}
 }
